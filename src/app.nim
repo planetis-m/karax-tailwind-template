d0c987f58541f37c4048939f62eb6a8b13f7cb74
Antonis Geralis <b3lievem@yandex.com>
2023-06-29 14:58:37 +0300

    remove cruft

--- src/app.nim
+++ src/app.nim
@@ -1,12 +1,9 @@
 include karax / prelude
 import std / dom
 
-template `^`(s: string): cstring = cstring(s)
-
-var clicked = false
 proc createDom(): VNode =
   result = buildHtml:
     h1(class="text-3xl font-bold underline"):
-      text ^"Hello world!"
+      text "Hello world!"
 
 setRenderer createDom
